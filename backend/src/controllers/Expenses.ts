1c8b41ccaa35e64e75847d080aa952001800bce3
Tomas Lopes <tomas050302@gmail.com>
2020-10-21 18:03:06 +0100

    feat(backend): backend setup

---
+++ backend/src/controllers/Expenses.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+
+export default {
+  async index(request: Request, response: Response) {},
+
+  async show(request: Request, response: Response) {},
+
+  async store(request: Request, response: Response) {}
+};
