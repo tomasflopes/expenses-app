bca995d337342cae8cb0a947333d6ccaee975748
Tomas Lopes <tomas050302@gmail.com>
2021-01-29 16:18:21 +0000

    feat(profile): added structure of profile page

--- mobile/src/screens/Profile/index.tsx
+++ mobile/src/screens/Profile/index.tsx
@@ -2,12 +2,42 @@ import React from 'react';
 
 import { Text } from 'react-native';
 
-import { Container } from './styles';
+import {
+  Container,
+  DeadZone,
+  Avatar,
+  UserInformationContainer,
+  Username,
+  Occupation,
+  PersonalInfoContainer,
+  Header,
+  InputContainer,
+  Label,
+  Input
+} from './styles';
 
 const Profile: React.FC = () => {
   return (
     <Container>
-      <Text>Profile</Text>
+      <DeadZone>
+        <Avatar
+          imageStyle={{ borderRadius: 120 / 2 }}
+          source={{ uri: 'http://www.github.com/tomas050302.png' }}
+        />
+        <UserInformationContainer>
+          <Username>Tomás Lopes</Username>
+          <Occupation>Student</Occupation>
+        </UserInformationContainer>
+
+        <PersonalInfoContainer>
+          <Header>Personal Info</Header>
+
+          <InputContainer>
+            <Label>First Name</Label>
+            <Input value={'Tomás'} />
+          </InputContainer>
+        </PersonalInfoContainer>
+      </DeadZone>
     </Container>
   );
 };
--- mobile/src/screens/Profile/styles.ts
+++ mobile/src/screens/Profile/styles.ts
@@ -1,3 +1,23 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View``;
+
+export const DeadZone = styled.View``;
+
+export const Avatar = styled.View``;
+
+export const UserInformationContainer = styled.View``;
+
+export const Username = styled.View``;
+
+export const Occupation = styled.View``;
+
+export const PersonalInfoContainer = styled.View``;
+
+export const Header = styled.View``;
+
+export const InputContainer = styled.View``;
+
+export const Label = styled.View``;
+
+export const Input = styled.View``;
