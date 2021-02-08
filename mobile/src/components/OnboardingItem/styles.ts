0e4c18b6b4ce009d5c8b6c7a74219cfd3cf36563
Tomas Lopes <tomas050302@gmail.com>
2021-02-08 12:06:39 +0000

    feat: added paginator animation

--- mobile/src/components/OnboardingItem/styles.ts
+++ mobile/src/components/OnboardingItem/styles.ts
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
 
-import theme from '../../../styles';
+import theme from '../../styles';
 
 const styles = StyleSheet.create({
   container: {
--- mobile/src/screens/Onboarding/index.tsx
+++ mobile/src/screens/Onboarding/index.tsx
@@ -1,9 +1,15 @@
 import React, { useState, useRef } from 'react';
 
-import { Animated, FlatList, Text, View } from 'react-native';
+import {
+  Animated,
+  FlatList,
+  Text,
+  View,
+  useWindowDimensions
+} from 'react-native';
+import { Extrapolate } from 'react-native-reanimated';
 
-import OnboardingItem from '../../components/Onboarding/OnboardingItem';
-import Paginator from '../../components/Onboarding/Paginator';
+import OnboardingItem from '../../components/OnboardingItem';
 
 import onboardingData from '../../utils/onboardingData';
 
@@ -12,6 +18,8 @@ import styles from './styles';
 const Onboarding: React.FC = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const scrollX = useRef(new Animated.Value(0)).current;
+
+  const { width } = useWindowDimensions();
 
   const slidesRef = useRef(null);
 
@@ -42,7 +50,28 @@ const Onboarding: React.FC = () => {
         ref={slidesRef}
       />
 
-      <Paginator data={onboardingData} scrollX={scrollX} />
+      <View style={styles.paginator}>
+        {onboardingData.map((item, index) => {
+          const inputRange = [
+            (index - 1) * width,
+            index * width,
+            (index + 1) * width
+          ];
+
+          const dotWidth = scrollX.interpolate({
+            inputRange,
+            outputRange: [10, 20, 10],
+            extrapolate: Extrapolate.CLAMP
+          });
+
+          return (
+            <Animated.View
+              key={index.toString()}
+              style={[styles.dot, { width: dotWidth }]}
+            />
+          );
+        })}
+      </View>
     </View>
   );
 };
