d70b1500bdff7662c3d434275166cb1a7d5431c2
Tomas Lopes <tomas050302@gmail.com>
2021-03-23 19:21:48 +0000

    refactor: switchable button

--- mobile/src/components/SwitchableButton/index.tsx
+++ mobile/src/components/SwitchableButton/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {
+  ColorValue,
   TouchableOpacity,
   TouchableOpacityProps,
   ViewStyle
 } from 'react-native';
 
 import { useTimingTransition } from 'react-native-redash';
-import Animated, { Extrapolate, interpolate } from 'react-native-reanimated';
+import Animated, { interpolateColors } from 'react-native-reanimated';
 
 import theme from '../../styles';
 import styles from './styles';
@@ -36,54 +37,33 @@ const SwitchableButton: React.FC<Props> = ({
 }) => {
   const transition = useTimingTransition(active, { duration: 500 });
 
-  const opacity = interpolate(transition, {
-    inputRange: [0, 1],
-    outputRange: [1, 0],
-    extrapolate: Extrapolate.CLAMP
-  });
+  // eslint-disable-next-line no-undef
+  const backgroundColor: AnimatedValue<ColorValue> = interpolateColors(
+    transition,
+    {
+      inputRange: [0, 1],
+      outputColorRange: [theme.colors[disabledColor], theme.colors[activeColor]]
+    }
+  );
 
-  const fontSize = interpolate(transition, {
-    inputRange: [0, 0.8, 1],
-    outputRange: [
-      theme.units.fontSize.bigHeader,
-      theme.units.fontSize.bigHeader + 0.5,
-      theme.units.fontSize.bigHeader
-    ],
-    extrapolate: Extrapolate.CLAMP
+  // eslint-disable-next-line no-undef
+  const color: AnimatedValue<ColorValue> = interpolateColors(transition, {
+    inputRange: [0, 1],
+    outputColorRange: [
+      theme.colors[disabledTextColor],
+      theme.colors[activeTextColor]
+    ]
   });
 
   return (
     <Animated.View style={[styles.buttonOverlay, { ...additionalStyles }]}>
-      <TouchableOpacity
-        ref={buttonRef}
-        onPress={handleSubmit}
-        style={[styles.button, { backgroundColor: theme.colors[activeColor] }]}
-      >
-        <Animated.Text
-          style={[
-            styles.buttonText,
-            { fontSize, color: theme.colors[activeTextColor] }
-          ]}
-        >
-          {children}
-        </Animated.Text>
+      <TouchableOpacity ref={buttonRef} onPress={handleSubmit}>
+        <Animated.View style={[styles.button, { backgroundColor }]}>
+          <Animated.Text style={[styles.buttonText, { color }]}>
+            {children}
+          </Animated.Text>
+        </Animated.View>
       </TouchableOpacity>
-
-      <Animated.View
-        style={[
-          styles.disabledButton,
-          { opacity, backgroundColor: theme.colors[disabledColor] }
-        ]}
-      >
-        <Animated.Text
-          style={[
-            styles.disabledButtonText,
-            { opacity, fontSize, color: theme.colors[disabledTextColor] }
-          ]}
-        >
-          {children}
-        </Animated.Text>
-      </Animated.View>
     </Animated.View>
   );
 };
